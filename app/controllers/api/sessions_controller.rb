9cca0ca9e28e2931207493a0432b27090db33eb0
jayad25 <dammalapati.jaya@gmail.com>
2018-12-11 14:06:15 -0800

    change render after login and signup user,add ajax request

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -11,7 +11,8 @@ class Api::SessionsController < ApplicationController
     end
     
     def destroy
-        if current_user
+        @user = current_user
+        if @user
             logout
             render "api/users/show"
         else
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,12 @@
 class Api::UsersController < ApplicationController
 
     def create
+        
         @user = User.new(user_params)
         debugger
         if @user.save
             login(@user)
-            render 'api/user/show'
+            render 'api/users/show'
         else
             render json:@user.errors.full_messages,status: 422
         end
