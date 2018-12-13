a9cf8036ed3d0d3380c590b6595adda96e10b1bd
jayad25 <dammalapati.jaya@gmail.com>
2018-12-12 16:10:36 -0800

    Prepare for initial Heroku push

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,7 +2,6 @@ class Api::SessionsController < ApplicationController
     def create
         @user = User.find_by_credentials(
             params[:user][:username],params[:user][:password])
-            # debugger
             if @user
                 login(@user)
                 
@@ -15,7 +14,6 @@ class Api::SessionsController < ApplicationController
     def destroy
         
         @user = current_user
-        # debugger
         if @user
             logout
             render "api/users/show"
